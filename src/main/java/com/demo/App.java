b9e6b8af7731295a85bdb164a7607f17837686e9
Kay Odenthal <kay.odenthal@gmx.de>
2015-09-16 14:30:37 +0200

    added xml mapping

--- src/main/java/com/demo/App.java
+++ src/main/java/com/demo/App.java
@@ -1,6 +1,7 @@
 package com.demo;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 /**
@@ -11,8 +12,10 @@ public class App
 {
     public static void main( String[] args )
     {
-        ApplicationContext context = new FileSystemXmlApplicationContext("/demo/src/main/java/com/demo/beans/beans.xml");
+        ApplicationContext context = new ClassPathXmlApplicationContext("com/demo/beans/beans.xml");
     	Person person = (Person) context.getBean("person");
         person.speak();
+        
+        System.out.println(person);
     }
 }
--- src/main/java/com/demo/Person.java
+++ src/main/java/com/demo/Person.java
@@ -1,8 +1,24 @@
 package com.demo;
 
 public class Person {
+	private int id;
+	private String name;
+
+	public Person() {
+	}
+
+	public Person(int id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
 	public void speak() {
 		System.out.println("hello world!");
+	}
+
+	@Override
+	public String toString() {
+		return "Person [id=" + id + ", name=" + name + "]";
 	}
 
 }
