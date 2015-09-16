74a78c9984b6057b23de140076a9dff6d1e7d1b3
Kay Odenthal <kay.odenthal@gmx.de>
2015-09-16 14:14:57 +0200

    lala

--- src/main/java/com/demo/App.java
+++ src/main/java/com/demo/App.java
@@ -11,7 +11,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        ApplicationContext context = new FileSystemXmlApplicationContext("beans.xml");
+        ApplicationContext context = new FileSystemXmlApplicationContext("/demo/src/main/java/com/demo/beans/beans.xml");
     	Person person = (Person) context.getBean("person");
         person.speak();
     }
